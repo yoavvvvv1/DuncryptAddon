78831d0ca271ba4aea1bd26baa2c7debb6a3464a
lidanthedev <ls301205@gmail.com>
2025-06-25 00:15:58 +0300

    Init

---
+++ src/main/java/me/lidan/griffinAddon/GriffinAddon.java
@@ -0,0 +1,17 @@
+package me.lidan.griffinAddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GriffinAddon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
